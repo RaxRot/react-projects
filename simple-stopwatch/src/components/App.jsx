4ae1c78046dcf9a0ec8257ce2a0a4dcddc98fb36
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-04-23 10:57:07 +0100

    micro fix

--- simple-stopwatch/src/components/App.jsx
+++ simple-stopwatch/src/components/App.jsx
@@ -23,7 +23,7 @@ function App() {
   <div className="timer-info">
   <span>{"0" + Math.floor((time/60000)%60)}:</span>
    <span>{"0" + Math.floor((time/1000)%60)}:</span>
-   <span>{"0" + Math.floor((time/10)%100)}:</span>
+   <span>{"0" + Math.floor((time/10)%100)}</span>
   </div>
   <div className="buttons">
    {running?(
